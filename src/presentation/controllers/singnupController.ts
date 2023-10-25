e8c230ed2c2b4942221699f61f0222db6b1dc14c
Bruno M. Moreira <bruno_moreira.19@outlook.com>
2023-10-25 20:39:09 -0300

    feat: add controllers interface signature

--- src/presentation/controllers/singnupController.ts
+++ src/presentation/controllers/singnupController.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from "../errors/missignParamError";
 import { badRequest } from "../helpers/httpHelper";
+import { Controller } from "../protocols/controller";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
-export class SignupController {
+export class SignupController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = [
       "name",
